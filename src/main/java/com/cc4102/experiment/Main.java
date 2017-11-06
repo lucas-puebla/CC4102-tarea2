e89afaf84202a41fb3f774983b975333370043fa
Lucas Puebla Silva <luigilucas@gmail.com>
2017-11-06 20:51:34 -0300

    Main y experiment con los casos para todos los tipos de arboles.

--- src/main/java/com/cc4102/experiment/Main.java
+++ src/main/java/com/cc4102/experiment/Main.java
@@ -4,7 +4,9 @@
 package com.cc4102.experiment;
 
 import com.cc4102.stringDict.LinearProbingHashingTree;
+import com.cc4102.stringDict.PatriciaTree;
 import com.cc4102.stringDict.StringDictionary;
+import com.cc4102.stringDict.TernarySearchTree;
 import com.cc4102.stringDict.experiment.Experiment;
 
 /**
@@ -20,13 +22,28 @@ public class Main {
     String text1 = "habia una vez un pato imaginario que se dio cuenta que la vida no tiene sentido";
     String text2 = "habia una vez un lobo feroz que no tenia ganas de vivir la vida sin sentido";
     
+    // lpht
     StringDictionary lpht1, lpht2;
     lpht1 = new LinearProbingHashingTree(8);
     lpht2 = new LinearProbingHashingTree(8);
     
+    // pt
+    StringDictionary pt1, pt2;
+    pt1 = new PatriciaTree();
+    pt2 = new PatriciaTree();
+    
+    // tst
+    StringDictionary tst1, tst2;
+    tst1 = new TernarySearchTree();
+    tst2 = new TernarySearchTree();
+    
     exp.preProcessText(text1, text2);
     
     exp.runTest(lpht1, lpht2, new LinearProbingHashingTree(8));
+    
+    exp.runTest(pt1, pt2, new PatriciaTree());
+    
+    exp.runTest(tst1, tst2, new TernarySearchTree());
   }
 
 }
